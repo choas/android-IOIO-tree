db694212a3f49830f4afb5d14d368446a2430049
U-INT\gregoril <gregoril@MSGN00916.int.root.msg.ag>
2014-09-09 22:42:45 +0200

    flash first LED (test code)

--- app/src/main/java/net/choas/android/ioiotree/IOIOTreeLooper.java
+++ app/src/main/java/net/choas/android/ioiotree/IOIOTreeLooper.java
@@ -3,6 +3,7 @@ package net.choas.android.ioiotree;
 import android.util.Log;
 
 import ioio.lib.api.IOIO;
+import ioio.lib.api.PwmOutput;
 import ioio.lib.api.exception.ConnectionLostException;
 import ioio.lib.util.IOIOLooper;
 
@@ -13,14 +14,27 @@ public class IOIOTreeLooper implements IOIOLooper {
 
     private static final String TAG = "IOIOTreeLooper";
 
+
+    PwmOutput led;
+    private int v = 0;
+
     @Override
     public void setup(IOIO ioio) throws ConnectionLostException, InterruptedException {
         Log.d(TAG, "setup");
+
+        led = ioio.openPwmOutput(10, 100);
+
     }
 
     @Override
     public void loop() throws ConnectionLostException, InterruptedException {
         Log.d(TAG, "loop");
+
+        led.setPulseWidth(v++);
+
+        if (v > 255) {
+            v = 0;
+        }
     }
 
     @Override
