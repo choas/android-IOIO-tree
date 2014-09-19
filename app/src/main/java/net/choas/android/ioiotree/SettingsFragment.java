950f94f896815608e95743449a6d4c8ee1862243
U-INT\gregoril <gregoril@MSGN00916.int.root.msg.ag>
2014-09-19 23:23:21 +0200

    ListPreference

--- app/src/main/java/net/choas/android/ioiotree/SettingsFragment.java
+++ app/src/main/java/net/choas/android/ioiotree/SettingsFragment.java
@@ -6,6 +6,7 @@ import android.os.Bundle;
 import android.app.Fragment;
 import android.preference.PreferenceActivity;
 import android.preference.PreferenceFragment;
+import android.preference.PreferenceManager;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -23,12 +24,11 @@ public class SettingsFragment extends PreferenceFragment {
         // Required empty public constructor
     }
 
-
-
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         addPreferencesFromResource(R.xml.preferences);
-//        addPreferencesFromResource(R.xml.fr);
     }
+
+
 }
